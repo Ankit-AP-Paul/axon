e81fe5c0bcce4e6cca01c06ce2304f639f76c4a1
ABHIRUP <abhirupsaha02@gmail.com>
2024-08-24 20:51:10 +0530

    user model update

---
+++ backend/src/routers/auth/index.ts
@@ -0,0 +1,72 @@
+import { verifySignature } from '@taquito/utils'
+import jwt from 'jsonwebtoken'
+import { Router } from "express"
+import { JWT_SECRET, prismaClient } from '../../config'
+
+
+const router = Router()
+
+
+router.post('/signin', async (req, res) => {
+    const { publicKey, signature } = req.body
+
+    const message = `Sign into axon on ${new Date().getDate()}.${new Date().getMonth()}.${new Date().getFullYear()}`
+    // const messageBytes = generateMessageWithMagicByte(message, 'pack')
+
+    // const isValid = verifySignature(messageBytes, publicKey, signature)
+
+    // if (!isValid) {
+    //     return res.status(411).json({
+    //         message: 'Incorrect signature'
+    //     })
+    // }
+
+
+    try {
+        const existingUser = await prismaClient.user.findFirst({
+            where: {
+                address: publicKey
+            }
+        })
+
+        if (existingUser) {
+            const token = jwt.sign({
+                userID: existingUser.id
+            },
+                JWT_SECRET,
+                { expiresIn: '1d' }
+            )
+
+            req.session!.axon = token
+            res.json({ message: 'Logged In' })
+        }
+        else {
+            const user = await prismaClient.user.create({
+                data: {
+                    address: publicKey
+                }
+            })
+
+            const token = jwt.sign({
+                userID: user.id
+            },
+                JWT_SECRET,
+                { expiresIn: '1d' }
+            )
+
+            req.session!.axon = token
+            res.json({ message: 'Logged In' })
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+router.get('/logout', async (req, res) => {
+    req.session = null
+    res.json({ message: 'Logged Out' })
+})
+
+
+export default router
