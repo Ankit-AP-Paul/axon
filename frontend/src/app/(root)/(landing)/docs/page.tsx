8d52cfc6f194a5e1f8028872605711a17eb622db
Ankit-AP-Paul <ankitpaul894@gmail.com>
2024-08-24 17:45:37 +0530

    rename and color changes

--- frontend/src/app/(root)/(landing)/docs/page.tsx
+++ frontend/src/app/(root)/(landing)/docs/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
-    return (
-        <div className='py-[3%] px-[5%]'>
-            <h2 className='text-3xl font-bold'>Docs for TAppIN</h2>
-        </div>
-    )
+  return (
+    <div className="py-[3%] px-[5%]">
+      <h2 className="text-3xl font-bold">Docs for Axon</h2>
+    </div>
+  );
 }
--- frontend/src/components/common/NavbarMain.tsx
+++ frontend/src/components/common/NavbarMain.tsx
@@ -2,7 +2,7 @@
 import { images } from "@/constants/images/images";
 import Image from "next/image";
 import Link from "next/link";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Menu, Wallet2 } from "lucide-react";
 import dynamic from "next/dynamic";
@@ -34,82 +34,82 @@ const messageToHexExpr = (message: string) => {
 
 Tezos.setWalletProvider(wallet);
 
-
 const NavbarMain = () => {
+  const [walletAddress, setWalletAddress] = useState<string | undefined>(
+    undefined
+  );
 
-    const [walletAddress, setWalletAddress] = useState<string | undefined>(
-        undefined
-      );
+  useEffect(() => {
+    const checkAccount = async () => {
+      const beaconActiveAccount = await wallet.client.getActiveAccount();
+      if (beaconActiveAccount) {
+        setWalletAddress(beaconActiveAccount.address);
+      }
+    };
 
-      useEffect(() => {
-        const checkAccount = async () => {
-          const beaconActiveAccount = await wallet.client.getActiveAccount();
-          if (beaconActiveAccount) {
-            setWalletAddress(beaconActiveAccount.address);
-          }
-        };
+    checkAccount();
+  }, []);
 
-        checkAccount();
-      }, []);
+  const connect = async () => {
+    const persmission = await wallet.client.requestPermissions();
+    setWalletAddress(persmission.address);
+    console.log(persmission);
+    const response = await wallet.client.requestSignPayload({
+      signingType: SigningType.MICHELINE,
+      payload: messageToHexExpr(
+        `Sign into axon on ${new Date().getDate()}.${new Date().getMonth()}.${new Date().getFullYear()}`
+      ),
+      sourceAddress: walletAddress,
+    });
 
-      const connect = async () => {
-        const persmission = await wallet.client.requestPermissions();
-        setWalletAddress(persmission.address);
-        console.log(persmission);
-        const response = await wallet.client.requestSignPayload({
-            signingType: SigningType.MICHELINE,
-            payload: messageToHexExpr(
-              `Sign into axon on ${new Date().getDate()}.${new Date().getMonth()}.${new Date().getFullYear()}`
-            ),
-            sourceAddress: walletAddress,
-          });
+    console.log(response.signature);
+  };
 
-          console.log(response.signature);
-      };
+  const disconnect = async () => {
+    await wallet.client.clearActiveAccount();
+    setWalletAddress(undefined);
+  };
 
-      const disconnect = async () => {
-        await wallet.client.clearActiveAccount();
-        setWalletAddress(undefined);
-      };
-
-
-
-    return (
-        <div className="sticky top-4 px-[5%]">
-            <nav className="px-[2%] rounded-md offsetEffect bg-[#b3a2e5]  py-2 border border-black flex justify-between items-center">
-                <div className="flex items-center gap-2">
-                    <Image
-                        src={images["log-black"]}
-                        height={40}
-                        width={40}
-                        alt="lgo"
-                    />
-                    <span className="font-bold text-lg">TAppIN™</span>
-                </div>
-                <div className="font-medium md:flex hidden gap-2 items-center w-max">
-                    <Link href="/">Home</Link>
-                    <Link href="/docs">Docs</Link>
-                    <Link href="/#faq">FAQs</Link>
-
-                    <Link href="/provider">Provider</Link>
-                    <Link href="/rent">Renting</Link>
-                    <Button
-                        asChild
-                        className="bg-white border hover:text-white border-black ml-2 text-black rounded-md offsetstyle"
-                    >
-                        <Link href="/onboard">Sign in</Link>
-                    </Button>
-
-                </div>
-                 <Button onClick={()=>{
-                    walletAddress===undefined?connect():disconnect();
-                }} >{walletAddress===undefined?"Connect":"disconnect"}</Button>
-                <Button className="aspect-square p-[10px] offsetstyle bg-white  text-black generalBorder md:hidden hover:text-white">
-                    <Menu size={30} />
-                </Button>
-            </nav>
+  return (
+    <div className="sticky top-4 px-[5%]">
+      <nav className="px-[2%] rounded-md offsetEffect bg-[#b3a2e5]  py-2 border border-black flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <Image src={images["log-black"]} height={40} width={40} alt="lgo" />
+          <span className="font-bold text-lg">Axon</span>
         </div>
-    );
-}
+        <div className="font-medium md:flex hidden gap-2 items-center w-max">
+          <Link href="/">Home</Link>
+          <Link href="/docs">Docs</Link>
+          <Link href="/#faq">FAQs</Link>
 
-export default dynamic(() => Promise.resolve(NavbarMain), { ssr: false })
+          <Link href="/provider">Provider</Link>
+          <Link href="/rent">Renting</Link>
+        </div>
+        <div>
+          <Button
+            // asChild
+            className="bg-white border hover:text-white border-black ml-2 text-black rounded-md offsetstyle">
+            <Link href="/onboard">Sign in</Link>
+          </Button>
+
+          <Button
+            className={
+              walletAddress === undefined
+                ? "bg-green-300 border hover:text-white border-black ml-2 text-black rounded-md offsetstyle"
+                : "bg-red-300 border hover:text-white border-black ml-2 text-black rounded-md offsetstyle"
+            }
+            onClick={() => {
+              walletAddress === undefined ? connect() : disconnect();
+            }}>
+            {walletAddress === undefined ? "Connect" : "Disconnect"}
+          </Button>
+        </div>
+        <Button className="aspect-square p-[10px] offsetstyle bg-white  text-black generalBorder md:hidden hover:text-white">
+          <Menu size={30} />
+        </Button>
+      </nav>
+    </div>
+  );
+};
+
+export default dynamic(() => Promise.resolve(NavbarMain), { ssr: false });
