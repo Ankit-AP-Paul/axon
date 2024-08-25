810f6abe0a0f364b243aba3794e6f77469e689d9
Ankit-AP-Paul <ankitpaul894@gmail.com>
2024-08-25 11:45:54 +0530

    integrated machine endpoints frontend

--- frontend/src/lib/apiCalls.ts
+++ frontend/src/lib/apiCalls.ts
@@ -1,132 +1,186 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL as string
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
-export async function userSignIn(address: string, publicKey: string, signature: string) {
-    try {
-        const res = await fetch(`${API_URL}/auth/signin`, {
-            method: 'POST',
-            body: JSON.stringify({
-                address,
-                publicKey,
-                signature
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include'
-        })
+export async function userSignIn(
+  address: string,
+  publicKey: string,
+  signature: string
+) {
+  try {
+    const res = await fetch(`${API_URL}/auth/signin`, {
+      method: "POST",
+      body: JSON.stringify({
+        address,
+        publicKey,
+        signature,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
 
-        const data = await res.json()
+    const data = await res.json();
 
-        return data.message
-    }
-    catch (err) {
-        console.error(err)
-    }
+    return data.message;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export async function logout() {
-    try {
-        const res = await fetch(`${API_URL}/auth/logout`, {
-            credentials: 'include'
-        })
+  try {
+    const res = await fetch(`${API_URL}/auth/logout`, {
+      credentials: "include",
+    });
 
-        const data = await res.json()
+    const data = await res.json();
 
-        return data.message
-    }
-    catch (err) {
-        console.error(err)
-    }
+    return data.message;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export async function getContractor() {
-    try {
-        const res = await fetch(`${API_URL}/auth/get-contractor`, {
-            credentials: 'include'
-        })
+  try {
+    const res = await fetch(`${API_URL}/auth/get-contractor`, {
+      credentials: "include",
+    });
 
-        const data = await res.json()
+    const data = await res.json();
 
-        return data.contractor
-    }
-    catch (err) {
-        console.error(err)
-    }
+    return data.contractor;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export async function getProvider() {
-    try {
-        const res = await fetch(`${API_URL}/auth/get-provider`, {
-            credentials: 'include'
-        })
+  try {
+    const res = await fetch(`${API_URL}/auth/get-provider`, {
+      credentials: "include",
+    });
 
-        const data = await res.json()
+    const data = await res.json();
 
-        return data.provider
-    }
-    catch (err) {
-        console.error(err)
-    }
+    return data.provider;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export async function contractorProfile(name: string, email: string) {
-    try {
-        const res = await fetch(`${API_URL}/auth/contractor-signup`, {
-            method: 'POST',
-            body: JSON.stringify({
-                name,
-                email
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        })
+  try {
+    const res = await fetch(`${API_URL}/auth/contractor-signup`, {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        email,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
 
-        const data = await res.json()
+    const data = await res.json();
 
-        return data.message
-    }
-    catch (err) {
-        console.error(err)
-    }
+    return data.message;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export async function providerProfile(name: string, email: string) {
-    try {
-        const res = await fetch(`${API_URL}/auth/provider-signup`, {
-            method: 'POST',
-            body: JSON.stringify({
-                name,
-                email
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        })
+  try {
+    const res = await fetch(`${API_URL}/auth/provider-signup`, {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        email,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
 
-        const data = await res.json()
+    const data = await res.json();
 
-        return data.message
-    }
-    catch (err) {
-        console.error(err)
-    }
+    return data.message;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-
 export async function getPresignedURL(filename: string) {
-    try {
-        const res = await fetch(`${API_URL}/store/presignedUrl?filename=${filename}`, {
-            credentials: 'include'
-        })
+  try {
+    const res = await fetch(
+      `${API_URL}/store/presignedUrl?filename=${filename}`,
+      {
+        credentials: "include",
+      }
+    );
 
-        const data = await res.json()
+    const data = await res.json();
 
-        return data
-    }
-    catch (err) {
-        console.error(err)
-    }
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getAllMachine() {
+  try {
+    const res = await fetch(`${API_URL}/machine/`);
+
+    const data = await res.json();
+
+    return data.machines;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function createMachine(
+  title: string,
+  cpu: number,
+  ram: number,
+  size: number
+) {
+  try {
+    const res = await fetch(`${API_URL}/machine/create`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        cpu,
+        ram,
+        size,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    return data.message;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getMachine() {
+  try {
+    const res = await fetch(`${API_URL}/machine/my-machines`, {
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    return data.machines;
+  } catch (err) {
+    console.error(err);
+  }
 }
