cdf0b60f6bbc0f3bfcb1824f00ee4a765c7979e6
ABHIRUP <abhirupsaha02@gmail.com>
2024-08-25 10:18:41 +0530

    machine ready

---
+++ backend/src/routers/machine/index.ts
@@ -0,0 +1,100 @@
+import { Router } from "express";
+import { authMiddleware } from "../../middleware";
+import { prismaClient } from "../../config";
+
+const router = Router()
+
+
+router.get('/', async (req, res) => {
+    try {
+        const machines = await prismaClient.machine.findMany()
+
+        res.status(200).json({ machines })
+    }
+    catch (err) {
+        console.log(err)
+
+        res.status(500).json({
+            message: 'Something wrong happened'
+        })
+    }
+})
+
+router.get('/my-machines', authMiddleware, async (req, res) => {
+    //@ts-ignore
+    const userID = req.userID
+    console.log(userID);
+
+    try {
+        const provider = await prismaClient.provider.findUnique({
+            where: {
+                user_id: userID
+            }
+        })
+
+        if (!provider) return res.status(404).json({
+            message: 'Provider not found'
+
+        })
+
+        const machines = await prismaClient.machine.findMany({
+            where: {
+                provider_id: provider.id
+            }
+        })
+
+        res.status(200).json({ machines })
+    }
+    catch (err) {
+        console.log(err)
+
+        res.status(500).json({
+            message: 'Something wrong happened'
+        })
+    }
+})
+
+router.post('/create', authMiddleware, async (req, res) => {
+    //@ts-ignore
+    const userID = req.userID
+    const { title, cpu, ram, size } = req.body
+
+    try {
+        const provider = await prismaClient.provider.findUnique({
+            where: {
+                user_id: userID
+            }
+        })
+
+        if (!provider) return res.status(404).json({
+            message: 'Provider not found'
+
+        })
+
+        const machine = await prismaClient.machine.create({
+            data: {
+                provider_id: provider.id,
+                title,
+                cpu,
+                ram,
+                size,
+            }
+        })
+
+        console.log(machine)
+
+        res.status(201).json({
+            message: 'Machine created'
+        })
+    }
+    catch (err) {
+        console.log(err)
+
+        res.status(500).json({
+            message: 'Something wrong happened'
+        })
+    }
+})
+
+
+export default router
---
+++ backend/src/routers/rent/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { authMiddleware } from "../../middleware";
+import { prismaClient } from "../../config";
+
+const router = Router()
+
+
+export default router
